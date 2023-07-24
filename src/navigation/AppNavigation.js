712840da168d22618eaea5abd6ef2ff34f1e0272
Fahad <fahhod.ahmad@gmail.com>
2023-07-24 10:29:26 +0300

    added addTrip

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -1,16 +1,78 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { getFocusedRouteNameFromRoute } from "@react-navigation/native"; // Import getFocusedRouteNameFromRoute
+
 import ROUTES from ".";
 import Explore from "../screens/Explore";
 import AddTrip from "../screens/AddTrip";
+import { Ionicons } from "@expo/vector-icons";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
 
 const Tab = createBottomTabNavigator();
 
+// const focusedRouteName = getFocusedRouteNameFromRoute(route);
+
 export default function AppNavigation() {
   return (
-    <Tab.Navigator screenOptions={{}}>
+    <Tab.Navigator
+      screenOptions={{
+        tabBarLabelStyle: {
+          display: "none", // Hide labels on focus for the "AddTrip" tab
+        },
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          position: "absolute",
+          bottom: 20,
+          left: 20,
+          right: 20,
+          elevation: 0,
+          backgroundColor: "#ffffff",
+          borderRadius: 15,
+          height: 70,
+          overflow: "hidden",
+          ...styles.shadow,
+        },
+      }}
+    >
       <Tab.Screen name={ROUTES.APPROUTES.EXPLORE} component={Explore} />
-      <Tab.Screen name={ROUTES.APPROUTES.ADD_TRIP} component={AddTrip} />
+      <Tab.Screen
+        options={({ route, focused }) => ({
+          tabBarIconStyle: {
+            backgroundColor:
+              route.name == ROUTES.APPROUTES.ADD_TRIP ? "blue" : "black",
+            borderRadius: 15,
+            width: 60,
+            marginVertical: 5,
+            ...styles.shadow,
+          },
+          headerShown: false,
+          tabBarShowLabel: () => null,
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="add" color={"white"} size={32} />
+          ),
+        })}
+        name={ROUTES.APPROUTES.ADD_TRIP}
+        component={AddTrip}
+      />
       <Tab.Screen name={ROUTES.APPROUTES.PROFILE} component={AddTrip} />
     </Tab.Navigator>
   );
 }
+
+const styles = StyleSheet.create({
+  shadow: {
+    shadowColor: "#7F5DF0",
+    shadowOffset: {
+      width: 0,
+      height: 10,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.5,
+    elevation: 5,
+  },
+});
--- src/screens/AddTrip.js
+++ src/screens/AddTrip.js
@@ -1,9 +1,33 @@
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function AddTrip() {
   return (
-    <View>
-      <Text>AddTrip</Text>
+    <View className="flex-1 bg-white justify-center items-center">
+      <Text className="text-xl font-bold absolute top-20">AddTrip</Text>
+      <View className=" w-[70%] ">
+        <Text className="mb-1">Title</Text>
+        <TextInput
+          className="bg-gray-100 rounded-xl mb-6 p-2"
+          placeholder="Title"
+        ></TextInput>
+        <Text className="mb-1">Description</Text>
+        <TextInput
+          multiline
+          className="bg-gray-100 rounded-xl p-2 mb-6"
+          placeholder="Description"
+        ></TextInput>
+        <Text className="mt-4 mb-1">Image</Text>
+        <TouchableOpacity className="bg-gray-100 rounded-xl items-center p-8">
+          <Ionicons name="add" size={52} color="green" />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
