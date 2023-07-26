61ef47cbce83cb2f40fabdc8624d9938c9f729ec
Bashayer770 <Beshow95@gmail.com>
2023-07-26 21:07:23 +0300

    linked trip details

--- src/screens/MyExplorePage.js
+++ src/screens/MyExplorePage.js
@@ -1,4 +1,11 @@
-import { View, Text, FlatList, RefreshControl } from "react-native";
+import {
+  View,
+  Text,
+  FlatList,
+  RefreshControl,
+  Image,
+  TouchableOpacity,
+} from "react-native";
 import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 
@@ -6,31 +13,52 @@ import Constants from "expo-constants";
 import { ScrollView } from "react-native";
 import { useQuery } from "@tanstack/react-query";
 import { getAllTrips } from "../apis/trips";
+import { BASE_URL } from "../apis";
+import ROUTES from "../navigation";
 
-export default function MyExplorePage() {
+export default function MyExplorePage({ navigation }) {
   const { data, isFetching, error, refetch } = useQuery({
     queryKey: ["trips"],
     queryFn: () => getAllTrips(),
   });
-
-  console.log(data);
+  //   console.log(data);
   const tripList = data?.map((trip) => {
-    return <Text key={trip._id}>{trip._id}</Text>;
+    console.log(`${BASE_URL}/${trip.image}`);
+    return (
+      <TouchableOpacity
+        key={trip._id}
+        className="w-[33%] h-60 mt-[-1.5%]"
+        onPress={() =>
+          navigation.navigate(ROUTES.APPROUTES.TRIP_DETAIL, { trip })
+        }
+      >
+        <Image
+          className="w-full h-full"
+          source={{
+            uri: `${BASE_URL}/${trip.image}`,
+          }}
+        />
+      </TouchableOpacity>
+    );
   });
 
   return (
     <View
       style={{
         flex: 1,
-        backgroundColor: "green",
-        paddingTop: Constants.statusBarHeight,
       }}
     >
       <ScrollView
-        style={{ backgroundColor: "red" }}
         refreshControl={
           <RefreshControl refreshing={isFetching} onRefresh={refetch} />
         }
+        // className="flex-1"
+        contentContainerStyle={{
+          flexWrap: "wrap",
+          flexDirection: "row",
+          gap: 1,
+          paddingTop: Constants.statusBarHeight,
+        }}
       >
         {tripList}
       </ScrollView>
