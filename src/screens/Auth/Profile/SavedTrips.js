fe133aa115e5d924612092a5573eb4efc9af138b
Fahad <fahhod.ahmad@gmail.com>
2023-07-29 20:27:56 +0300

    fixes

---
+++ src/screens/Auth/Profile/SavedTrips.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const SavedTrips = () => {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-white ">Saved Trips</Text>
+    </View>
+  );
+};
+
+export default SavedTrips;
--- src/screens/TripDetail.js
+++ src/screens/TripDetail.js
@@ -255,14 +255,14 @@ const styles = StyleSheet.create({
 
     borderRadius: 10,
     // backgroundColor: "#fff",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
+    // shadowColor: "#000",
+    // shadowOffset: {
+    //   width: 0,
+    //   height: 2,
+    // },
+    // shadowOpacity: 0.25,
+    // shadowRadius: 3.84,
+    // elevation: 5,
   },
   userContainer: {
     flexDirection: "row",
